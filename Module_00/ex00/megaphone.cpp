fded2de379d688fc4d2e59d8e314285a339a453d
Ekkoz <apereira@student.42.fr>
2023-12-26 08:28:47 -0500

    started the project, finished ex00

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 07:24:04 by apereira          #+#    #+#             */
+/*   Updated: 2023/12/26 08:07:56 by apereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int	i = 1;
+	int	j = 0;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			if (i < (argc - 1))
+				std::cout << " ";
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
