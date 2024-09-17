bc8677cd0b6920a5fef88a4e59d05a6bd9028fd3
Ekkoz <abelix897@gmail.com>
2024-09-17 08:54:40 -0400

    FInished 06 && 07

--- Module_07/ex01/Iter.hpp
+++ Module_07/ex01/Iter.hpp
@@ -22,4 +22,33 @@ void    print(T value)
     std::cout << value << std::endl;
 }
 
+template <typename T>
+void print(const  T &value)
+{
+  std::cout << value << std::endl;
+}
+
+// // CODE PROVIDED IN EX01.CPP CORRECTION SHEET
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print(T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
 #endif
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -15,3 +15,13 @@ int main(void)
 	iter(d, 3, print);
     return (0);
 }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
